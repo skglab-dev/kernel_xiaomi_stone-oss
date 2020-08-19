32284ebd831b8e1a2f4c61006a85b0da7531b962
Stefan Hajnoczi <stefanha@redhat.com>
2020-08-19 18:19:49 -0400

    UPSTREAM: virtiofs: implement FUSE_INIT map_alignment field
    The device communicates FUSE_SETUPMAPPING/FUSE_REMOVMAPPING alignment
    constraints via the FUST_INIT map_alignment field.  Parse this field and
    ensure our DAX mappings meet the alignment constraints.
    
    We don't actually align anything differently since our mappings are
    already 2MB aligned.  Just check the value when the connection is
    established.  If it becomes necessary to honor arbitrary alignments in
    the future we'll have to adjust how mappings are sized.
    
    The upshot of this commit is that we can be confident that mappings will
    work even when emulating x86 on Power and similar combinations where the
    host page sizes are different.
    
    Change-Id: I8c7b002aa413dbdae43dc159d82cba672e671351
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1154,5 +1154,6 @@ ssize_t fuse_passthrough_mmap(struct file *file, struct vm_area_struct *vma);
 /* dax.c */
 int fuse_dax_conn_alloc(struct fuse_conn *fc, struct dax_device *dax_dev);
 void fuse_dax_conn_free(struct fuse_conn *fc);
+bool fuse_dax_check_alignment(struct fuse_conn *fc, unsigned int map_alignment);
 
 #endif /* _FS_FUSE_I_H */
