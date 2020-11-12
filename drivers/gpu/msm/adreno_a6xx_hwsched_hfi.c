f3b5f9488ec152b88b2a27631827b86c7d964c0f
Jordan Crouse <jcrouse@codeaurora.org>
2020-11-12 16:33:10 -0700

    msm: kgsl: Cleanup hwsched preempt count
    hwsched_preempt_count_get is only called from one place so fix the
    return value so it is a pass through.
    
    Change-Id: Ic0dedbadb2f76e405b37226206fbd93560b22c18
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
@@ -1366,7 +1366,7 @@ void a6xx_hwsched_context_detach(struct adreno_context *drawctxt)
 	mutex_unlock(&device->mutex);
 }
 
-int a6xx_hwsched_preempt_count_get(struct adreno_device *adreno_dev)
+u32 a6xx_hwsched_preempt_count_get(struct adreno_device *adreno_dev)
 {
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	struct hfi_get_value_cmd cmd;
@@ -1400,5 +1400,5 @@ int a6xx_hwsched_preempt_count_get(struct adreno_device *adreno_dev)
 done:
 	del_waiter(hfi, &pending_ack);
 
-	return rc ? rc : pending_ack.results[2];
+	return rc ? 0 : pending_ack.results[2];
 }
