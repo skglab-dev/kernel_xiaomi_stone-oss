09c165cbba5a7ab09d09055d1a6a658a914dd706
Christoph Hellwig <hch@lst.de>
2021-01-26 15:52:33 +0100

    blk-crypto: use bio_kmalloc in blk_crypto_clone_bio
    Use bio_kmalloc instead of open coding it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Acked-by: Damien Le Moal <damien.lemoal@wdc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- block/blk-crypto-fallback.c
+++ block/blk-crypto-fallback.c
@@ -171,7 +171,7 @@ static struct bio *blk_crypto_clone_bio(struct bio *bio_src)
 	struct bio_vec bv;
 	struct bio *bio;
 
-	bio = bio_alloc_bioset(GFP_NOIO, bio_segments(bio_src), NULL);
+	bio = bio_kmalloc(GFP_NOIO, bio_segments(bio_src));
 	if (!bio)
 		return NULL;
 	bio->bi_disk		= bio_src->bi_disk;
