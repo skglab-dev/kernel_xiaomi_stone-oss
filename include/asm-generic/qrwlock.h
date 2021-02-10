23545bc95f3914936803321c6af33e28ec9d13c4
Waiman Long <longman@redhat.com>
2021-02-10 13:16:31 -0500

    locking/arch: Move qrwlock.h include after qspinlock.h
    include/asm-generic/qrwlock.h was trying to get arch_spin_is_locked via
    asm-generic/qspinlock.h.  However, this does not work because architectures
    might be using queued rwlocks but not queued spinlocks (csky), or because they
    might be defining their own queued_* macros before including asm/qspinlock.h.
    
    To fix this, ensure that asm/spinlock.h always includes qrwlock.h after
    defining arch_spin_is_locked (either directly for csky, or via
    asm/qspinlock.h for other architectures).  The only inclusion elsewhere
    is in kernel/locking/qrwlock.c.  That one is really unnecessary because
    the file is only compiled in SMP configurations (config QUEUED_RWLOCKS
    depends on SMP) and in that case linux/spinlock.h already includes
    asm/qrwlock.h if needed, via asm/spinlock.h.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Waiman Long <longman@redhat.com>
    Fixes: 26128cb6c7e6 ("locking/rwlocks: Add contention detection for rwlocks")
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Ben Gardon <bgardon@google.com>
    [Add arch/sparc and kernel/locking parts per discussion with Waiman. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- include/asm-generic/qrwlock.h
+++ include/asm-generic/qrwlock.h
@@ -16,6 +16,8 @@
 #include <asm-generic/qrwlock_types.h>
 #include <asm-generic/qspinlock.h>
 
+/* Must be included from asm/spinlock.h after defining arch_spin_is_locked.  */
+
 /*
  * Writer states & reader shift and bias.
  */
