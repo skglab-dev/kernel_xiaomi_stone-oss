191082b949d495c195e8a72f2deae913da1f2a20
Juhyung Park <qkrwngud825@gmail.com>
2021-06-21 00:48:27 +0900

    power: reset: qcom-dload-mode: perform a warm, nodump reboot upon a panic
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -279,6 +279,12 @@ static int qcom_dload_panic(struct notifier_block *this, unsigned long event,
 	poweroff->in_panic = true;
 	if (enable_dump)
 		msm_enable_dump_mode(true);
+
+	// Perform a warm reboot
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	reboot_mode = REBOOT_WARM;
+	mb();
+
 	return NOTIFY_OK;
 }
 
