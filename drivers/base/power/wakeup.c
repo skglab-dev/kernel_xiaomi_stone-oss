37ed2dbc2d7ef202729120861d1d74861ead8ca0
Cyber Knight <cyberknight755@gmail.com>
2022-07-06 00:49:18 +0800

    base/power: wakeup: Add another hacked tracing/trace_events/ node
    - To silence logspams regarding atrace.
    
    Suggested-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Change-Id: I98d9aeccb7c789edfdbca22721c46136d42066a3
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: kawaaii <kawaaii@nocturn9x.space>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -1212,6 +1212,7 @@ static int __init wakeup_sources_debugfs_init(void)
 	debugfs_create_file("wakeup_sources", S_IRUGO, NULL, NULL,
 			    &wakeup_sources_stats_fops);
 	debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), &flag, NULL);
+        debugfs_create_dir("events",debugfs_create_dir("tracing", NULL));
 	return 0;
 }
 
