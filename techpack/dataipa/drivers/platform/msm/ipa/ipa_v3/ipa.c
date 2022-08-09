d4c9f1d1c96f87a799e1453bf8a3b4c0c61849a2
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-09 05:15:05 +0000

    techpack: dataipa: Conditionally enable ipa wakelocks
    These contribute to a massive amounts of idle drains.
    Stub out the wakelock generating function.
    
    Change-Id: I277cb6c67c491f7454377bb1f4cfd8214c45f086
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -5839,6 +5839,7 @@ void ipa3_dec_client_disable_clks_delay_wq(
 		&ipa_dec_clients_disable_clks_on_suspend_irq_wq_work, delay))
 		IPAERR("Scheduling delayed work failed\n");
 }
+#ifndef CONFIG_DISABLE_IPA_WAKELOCKS
 /**
  * ipa3_inc_acquire_wakelock() - Increase active clients counter, and
  * acquire wakelock if necessary
@@ -5879,6 +5880,16 @@ void ipa3_dec_release_wakelock(void)
 		__pm_relax(ipa3_ctx->w_lock);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
 }
+#else
+inline void ipa3_inc_acquire_wakelock(void)
+{
+	pr_debug("%s: Stub ipa wakelock fn\n", __func__);
+}
+inline void ipa3_dec_release_wakelock(void)
+{
+	pr_debug("%s: Stub ipa wakelock fn\n", __func__);
+}
+#endif
 
 int ipa3_set_clock_plan_from_pm(int idx)
 {
@@ -7823,6 +7834,7 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		goto fail_device_create;
 	}
 
+#ifndef CONFIG_DISABLE_IPA_WAKELOCKS
 	/* Register a wakeup source. */
 	ipa3_ctx->w_lock =
 		wakeup_source_register(&ipa_pdev->dev, "IPA_WS");
@@ -7832,6 +7844,7 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		goto fail_w_source_register;
 	}
 	spin_lock_init(&ipa3_ctx->wakelock_ref_cnt.spinlock);
+#endif
 
 	/* Initialize Power Management framework */
 	result = ipa_pm_init(&ipa3_res.pm_init);
@@ -7946,11 +7959,13 @@ fail_gsi_pre_fw_load_init:
 	ipa3_dma_shutdown();
 fail_ipa_dma_setup:
 	ipa_pm_destroy();
-fail_w_source_register:
-	device_destroy(ipa3_ctx->cdev.class, ipa3_ctx->cdev.dev_num);
 fail_ipa_pm_init:
+#ifndef CONFIG_DISABLE_IPA_WAKELOCKS
 	wakeup_source_unregister(ipa3_ctx->w_lock);
 	ipa3_ctx->w_lock = NULL;
+fail_w_source_register:
+#endif
+	device_destroy(ipa3_ctx->cdev.class, ipa3_ctx->cdev.dev_num);
 fail_device_create:
 	unregister_chrdev_region(ipa3_ctx->cdev.dev_num, 1);
 fail_alloc_chrdev_region:
