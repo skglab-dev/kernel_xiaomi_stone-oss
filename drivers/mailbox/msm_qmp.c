6ed08ec589b228467aae9dc069431bb7315f0502
Richard Maina <quic_rmaina@quicinc.com>
2022-10-26 09:13:36 -0700

    mailbox: msm_qmp: null check before dereference
    Including null check before pointer dereference to santize arguments
    from exported callback function.
    
    Change-Id: I90875b0dcd6bb8c4ddffb0d7c2da9a1d19e06fab
    Signed-off-by: Richard Maina <quic_rmaina@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/mailbox/msm_qmp.c
+++ drivers/mailbox/msm_qmp.c
@@ -382,7 +382,7 @@ static int qmp_startup(struct mbox_chan *chan)
  */
 static int qmp_send_data(struct mbox_chan *chan, void *data)
 {
-	struct qmp_mbox *mbox = chan->con_priv;
+	struct qmp_mbox *mbox;
 	struct qmp_device *mdev;
 	struct qmp_pkt *pkt = (struct qmp_pkt *)data;
 	void __iomem *addr;
@@ -390,7 +390,11 @@ static int qmp_send_data(struct mbox_chan *chan, void *data)
 	u32 size;
 	int i;
 
-	if (!mbox || !data || !completion_done(&mbox->ch_complete))
+	if (!chan || !data)
+		return -EINVAL;
+
+	mbox = chan->con_priv;
+	if (!mbox || !completion_done(&mbox->ch_complete))
 		return -EINVAL;
 
 	mdev = mbox->mdev;
