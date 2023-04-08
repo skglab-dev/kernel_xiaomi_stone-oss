1104618b3d7107f7133a427e8fefc36702af2bca
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-04-09 02:50:33 +0600

    techpack: dataipa: rmnet_ipa: Do not print iface name and quota size
    iface name and quota size are printed continuously and they are quite pesky.
    We don't need this info while using USB tethering. So, silence it.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -3301,7 +3301,7 @@ static int rmnet_ipa3_set_data_quota_modem(
 	data->interface_name[IFNAMSIZ-1] = '\0';
 
 	index = find_vchannel_name_index(data->interface_name);
-	IPAWANERR("iface name %s, quota %lu\n",
+	IPAWANDBG("iface name %s, quota %lu\n",
 		  data->interface_name, (unsigned long) data->quota_mbytes);
 
 	if (index == MAX_NUM_OF_MUX_CHANNEL) {
