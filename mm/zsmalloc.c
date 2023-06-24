3f11320f857d7a3a19aa901de7a2fc98e9ae2005
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-06-24 14:12:14 +0900

    FROMGIT: zsmalloc: do not scan for allocated objects in empty zspage
    Patch series "zsmalloc: small compaction improvements", v2.
    
    A tiny series that can reduce the number of find_alloced_obj() invocations
    (which perform a linear scan of sub-page) during compaction.  Inspired by
    Alexey Romanov's findings.
    
    This patch (of 3):
    
    zspage migration can terminate as soon as it moves the last allocated
    object from the source zspage.  Add a simple helper zspage_empty() that
    tests zspage ->inuse on each migration iteration.
    
    Link: https://lkml.kernel.org/r/20230624053120.643409-2-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Suggested-by: Alexey Romanov <AVRomanov@sberdevices.ru>
    Reviewed-by: Alexey Romanov <avromanov@sberdevices.ru>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 4d684ccb978e026aaf57cbd1b9f2d92d515e3289
     git://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm mm-stable)
    
    BUG=none
    TEST=Host memory pressure tast on brya
    
    Change-Id: I9a4aa21bf61ebd4c5346e85f6917e99aac66533f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4667085
    Tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1157,6 +1157,11 @@ static bool zspage_full(struct size_class *class, struct zspage *zspage)
 	return get_zspage_inuse(zspage) == class->objs_per_zspage;
 }
 
+static bool zspage_empty(struct zspage *zspage)
+{
+	return get_zspage_inuse(zspage) == 0;
+}
+
 unsigned long zs_get_total_pages(struct zs_pool *pool)
 {
 	return atomic_long_read(&pool->pages_allocated);
@@ -1605,6 +1610,10 @@ static void migrate_zspage(struct zs_pool *pool, struct size_class *class,
 		obj_idx++;
 		record_obj(handle, free_obj);
 		obj_free(class->size, used_obj);
+
+		/* Stop if there are no more objects to migrate */
+		if (zspage_empty(get_zspage(s_page)))
+			break;
 	}
 
 	/* Remember last position in this iteration */
