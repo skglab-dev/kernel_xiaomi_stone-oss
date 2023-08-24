7ce826509cbdcd3301d2b60cd8fb9e7f9ccb0837
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-08-24 05:30:18 +0000

    BACKPORT: UPSTREAM: zsmalloc: fix races between modifications of fullness and isolated
    We encountered many kernel exceptions of VM_BUG_ON(zspage->isolated ==
    0) in dec_zspage_isolation() and BUG_ON(!pages[1]) in zs_unmap_object()
    lately.  This issue only occurs when migration and reclamation occur at
    the same time.
    
    With our memory stress test, we can reproduce this issue several times
    a day.  We have no idea why no one else encountered this issue.  BTW,
    we switched to the new kernel version with this defect a few months
    ago.
    
    Since fullness and isolated share the same unsigned int, modifications of
    them should be protected by the same lock.
    
    [andrew.yang@mediatek.com: move comment]
      Link: https://lkml.kernel.org/r/20230727062910.6337-1-andrew.yang@mediatek.com
    Link: https://lkml.kernel.org/r/20230721063705.11455-1-andrew.yang@mediatek.com
    Fixes: c4549b871102 ("zsmalloc: remove zspage isolation for migration")
    Signed-off-by: Andrew Yang <andrew.yang@mediatek.com>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    [senozhatsky@: the race condition is between paths that do
      inc_zspage_isolation()/dec_zspage_isolation() and set_zspage_mapping(),
      which are performed under zspage migration lock, and paths that call
      fix_fullness_group(), which is executed under class->lock]
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    (cherry picked from commit 4b5d1e47b69426c0f7491d97d73ad0152d02d437)
    
    BUG=none
    TEST=Host MemoryPressure tast on brya
    
    Change-Id: Ieb05245ca381b6c42f0bc14b7dd8564d069cf7c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4809403
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -260,6 +260,7 @@ struct zspage {
 	unsigned int freeobj;
 	struct page *first_page;
 	struct list_head list; /* fullness list */
+	struct zs_pool *pool;
 #ifdef CONFIG_COMPACTION
 	rwlock_t lock;
 #endif
@@ -1026,6 +1027,7 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 
 	create_page_chain(class, zspage, pages);
 	init_zspage(class, zspage);
+	zspage->pool = pool;
 
 	return zspage;
 }
@@ -1795,6 +1797,7 @@ static void replace_sub_page(struct size_class *class, struct zspage *zspage,
 
 static bool zs_page_isolate(struct page *page, isolate_mode_t mode)
 {
+	struct size_class *class;
 	struct zspage *zspage;
 
 	/*
@@ -1805,10 +1808,10 @@ static bool zs_page_isolate(struct page *page, isolate_mode_t mode)
 	VM_BUG_ON_PAGE(PageIsolated(page), page);
 
 	zspage = get_zspage(page);
-	migrate_write_lock(zspage);
+	class = zspage_class(zspage->pool, zspage);
+	spin_lock(&class->lock);
 	inc_zspage_isolation(zspage);
-	migrate_write_unlock(zspage);
-
+	spin_unlock(&class->lock);
 	return true;
 }
 
@@ -1882,8 +1885,8 @@ static int zs_page_migrate(struct address_space *mapping, struct page *newpage,
 	 * it's okay to release migration_lock.
 	 */
 	write_unlock(&pool->migrate_lock);
-	spin_unlock(&class->lock);
 	dec_zspage_isolation(zspage);
+	spin_unlock(&class->lock);
 	migrate_write_unlock(zspage);
 
 	get_page(newpage);
@@ -1900,15 +1903,17 @@ static int zs_page_migrate(struct address_space *mapping, struct page *newpage,
 
 static void zs_page_putback(struct page *page)
 {
+	struct size_class *class;
 	struct zspage *zspage;
 
 	VM_BUG_ON_PAGE(!PageMovable(page), page);
 	VM_BUG_ON_PAGE(!PageIsolated(page), page);
 
 	zspage = get_zspage(page);
-	migrate_write_lock(zspage);
+	class = zspage_class(zspage->pool, zspage);
+	spin_lock(&class->lock);
 	dec_zspage_isolation(zspage);
-	migrate_write_unlock(zspage);
+	spin_unlock(&class->lock);
 }
 
 static const struct address_space_operations zsmalloc_aops = {
