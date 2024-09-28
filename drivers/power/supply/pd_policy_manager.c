81f310cb3eeb9e268cc34298bb6ff131b3d5a151
Giovanni Ricca <giovanniricca@proton.me>
2024-09-28 16:45:27 +0200

    power: supply: pd_policy_manager: Fix `-Wmisleading-indentation` warning
    Change-Id: Ic7fb24caafe983b2dac1b1f923421fd628eaed1d
    Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

--- drivers/power/supply/pd_policy_manager.c
+++ drivers/power/supply/pd_policy_manager.c
@@ -807,7 +807,7 @@ static int  get_batt_temp_thermal_curr(struct usbpd_pm *pdpm)
     if (!ret)
         pdpm->therm_curr = thermal_mitigation[val.intval]/1000;
 
-        return 0;
+    return 0;
 }
 
 extern int get_usbpd_verifed_state(void);
@@ -1197,7 +1197,7 @@ static void usbpd_pd_contact(struct usbpd_pm *pdpm, bool connected)
             schedule_delayed_work(&pdpm->pm_work, 0);
         else
             pdpm->pd_active = false;
-            power_supply_changed(pdpm->usb_psy);
+        power_supply_changed(pdpm->usb_psy);
     } else {
         usbpd_pm_disconnect(pdpm);
     }
