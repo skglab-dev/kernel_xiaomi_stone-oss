55e12989761e91ecee79fedb96b6f947aa2d34c0
Mirah Syakilla <mirahsyakilla@gmail.com>
2025-12-29 01:28:55 +0800

    f2fs: optimize garbage collection for reduced latency
    This patch optimizes the GC logic to prevent UI jank and stalls:
    1. Limit victim search in Urgent Mode: Prevents the GC thread from
       scanning the entire disk during critical low-space situations,
       which previously caused massive latency spikes.
    2. Fix sleep time in Urgent Mode: If GC fails to find a victim in
       Urgent Mode, retry quickly (5ms) instead of sleeping for 5 minutes,
       preventing "Out of Space" errors.
    
    Change-Id: I59fc94338b2edff060692383e2706ec7d11ef716
    Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -108,8 +108,13 @@ do_gc:
 		sync_mode = F2FS_OPTION(sbi).bggc_mode == BGGC_MODE_SYNC;
 
 		/* if return value is not zero, no victim was selected */
-		if (f2fs_gc(sbi, sync_mode, true, NULL_SEGNO))
-			wait_ms = gc_th->no_gc_sleep_time;
+		if (f2fs_gc(sbi, sync_mode, true, NULL_SEGNO)) {
+			if (sbi->gc_mode == GC_URGENT)
+				wait_ms = gc_th->urgent_sleep_time;
+			else
+				wait_ms = gc_th->no_gc_sleep_time;
+				break;
+		}
 
 		trace_f2fs_background_gc(sbi->sb, wait_ms,
 				prefree_segments(sbi), free_segments(sbi));
@@ -207,7 +212,6 @@ static void select_policy(struct f2fs_sb_info *sbi, int gc_type,
 	 * foreground GC and urgent GC cases.
 	 */
 	if (gc_type != FG_GC &&
-			(sbi->gc_mode != GC_URGENT) &&
 			p->max_search > sbi->max_victim_search)
 		p->max_search = sbi->max_victim_search;
 
