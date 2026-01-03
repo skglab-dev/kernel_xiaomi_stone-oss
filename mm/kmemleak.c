996326139fe78f3840ecf1e274a4531900a30b61
Sultan Alsawaf <sultan@kerneltoast.com>
2026-01-03 08:48:11 +0700

    mm: kmemleak: Don't die when memory allocation fails
    When memory is leaking, it's going to be harder to allocate more memory,
    making it more likely for this failure condition inside of kmemleak to
    manifest itself. This is extremely frustrating since kmemleak kills
    itself upon the first instance of memory allocation failure.
    
    Bypass that and make kmemleak more resilient when memory is running low.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I67fc2b35576b6aacc1b9eb2870af45ee22e5f9a2
    Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -579,11 +579,10 @@ static struct kmemleak_object *create_object(unsigned long ptr, size_t size,
 	struct rb_node **link, *rb_parent;
 	unsigned long untagged_ptr;
 
-	object = mem_pool_alloc(gfp);
-	if (!object) {
-		pr_warn("Cannot allocate a kmemleak_object structure\n");
-		kmemleak_disable();
-		return NULL;
+	while (1) {
+		object = kmem_cache_alloc(object_cache, gfp_kmemleak_mask(gfp));
+		if (object)
+			break;
 	}
 
 	INIT_LIST_HEAD(&object->object_list);
