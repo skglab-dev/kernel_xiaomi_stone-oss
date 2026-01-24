0e089bb7c071fa0841044e3a943e6a0083d037c3
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2026-01-24 10:25:00 +0000

    block: get rid of the trace rq insert wrapper
    Get rid of the wrapper for trace_block_rq_insert() and call the function
    directly.
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: Damien Le Moal <damien.lemoal@wdc.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -124,6 +124,8 @@
 #include <linux/sbitmap.h>
 #include <linux/delay.h>
 
+#include <trace/events/block.h>
+
 #include "blk.h"
 #include "blk-mq.h"
 #include "blk-mq-tag.h"
@@ -5641,7 +5643,7 @@ static void bfq_insert_request(struct blk_mq_hw_ctx *hctx, struct request *rq,
 		return;
 	}
 
-	blk_mq_sched_request_inserted(rq);
+	trace_block_rq_insert(rq);
 
 	if (!bfqq || at_head || blk_rq_is_passthrough(rq)) {
 		if (at_head)
--- block/mq-deadline-main.c
+++ block/mq-deadline-main.c
@@ -18,6 +18,8 @@
 #include <linux/rbtree.h>
 #include <linux/sbitmap.h>
 
+#include <trace/events/block.h>
+
 #include "blk.h"
 #include "blk-mq.h"
 #include "blk-mq-debugfs.h"
@@ -744,7 +746,7 @@ static void dd_insert_request(struct blk_mq_hw_ctx *hctx, struct request *rq,
 	if (blk_mq_sched_try_insert_merge(q, rq))
 		return;
 
-	blk_mq_sched_request_inserted(rq);
+	trace_block_rq_insert(rq);
 
 	per_prio = &dd->per_prio[prio];
 	if (at_head) {
