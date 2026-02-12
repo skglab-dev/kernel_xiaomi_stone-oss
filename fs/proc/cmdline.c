a93aed4cfd3e267cb80140a4a0e851875d9971d2
Mornye <mornye150190@gmail.com>
2026-02-12 06:07:07 +0000

    fs: proc: spoof device as LBL
    Change-Id: I76d4a255c3b7e524f3f1758fb63ff9f808583e5b

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,11 +3,38 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/string.h>
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_puts(m, saved_command_line);
-	seq_putc(m, '\n');
+	char *c;
+	char *s;
+
+	c = kstrdup(saved_command_line, GFP_KERNEL);
+	if (!c) {
+		seq_puts(m, saved_command_line);
+		seq_putc(m, '\n');
+	return 0;
+	}
+
+	s = strstr(c, "androidboot.verifiedbootstate=orange");
+	if (s) {
+		memcpy(s + 30, "green ", 6);
+	}
+
+	s = strstr(c, "androidboot.vbmeta.device_state=unlocked");
+	if (s) {
+		memcpy(s + 32, "locked  ", 8);
+	}
+
+	s = strstr(c, "androidboot.selinux=permissive");
+	if (s) {
+		memcpy(s + 20, "enforcing ", 10);
+	}
+
+	seq_printf(m, "%s\n", c);
+	kfree(c);
 	return 0;
 }
 
