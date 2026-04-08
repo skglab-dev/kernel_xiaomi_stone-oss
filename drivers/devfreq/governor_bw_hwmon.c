73bc6b9c4d78ebdad4bbef4a600310eaad2a6093
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-04-09 06:04:12 +0800

    devfreq: tune bw_hwmon defaults for multicore throughput

--- drivers/devfreq/governor_bw_hwmon.c
+++ drivers/devfreq/governor_bw_hwmon.c
@@ -981,18 +981,18 @@ int register_bw_hwmon(struct device *dev, struct bw_hwmon *hwmon)
 		node->attr_grp = &dev_attr_group;
 	}
 
-	node->guard_band_mbps = 100;
-	node->decay_rate = 90;
+	node->guard_band_mbps = 150;
+	node->decay_rate = 80;
 	node->io_percent = 16;
 	node->bw_step = 190;
-	node->sample_ms = 50;
-	node->up_scale = 0;
-	node->up_thres = 10;
+	node->sample_ms = 20;
+	node->up_scale = 25;
+	node->up_thres = 5;
 	node->down_thres = 0;
-	node->down_count = 3;
-	node->hist_memory = 0;
+	node->down_count = 5;
+	node->hist_memory = 4;
 	node->hyst_trigger_count = 3;
-	node->hyst_length = 0;
+	node->hyst_length = 4;
 	node->idle_mbps = 400;
 	node->use_ab = 1;
 	node->mbps_zones[0] = 0;
