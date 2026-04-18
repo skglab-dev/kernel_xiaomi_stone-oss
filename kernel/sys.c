fadd726ad5ed46022baf3c10f7ddf084bfb11b27
Mirah Syakilla <mirahsyakilla@gmail.com>
2026-04-18 10:10:14 +0800

    kernel/sys: Properly spoof uname to avoid detection
    Previous spoof method was being detected since its
    not also checking uid. This commit resolve those.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1248,9 +1248,10 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
+	if (uid_eq(current_uid(), GLOBAL_ROOT_UID) &&
+	    (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4))) {
 		strcpy(tmp.release, "5.10.239");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
