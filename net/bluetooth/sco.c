59714dd268705bc2c038d89f9b3a650199ebaf2d
Muhammad Ibnu Sina Rasyid Al Kautsar <rasyid2007alkautsar@gmail.com>
2026-05-08 07:11:38 +0000

    bluetooth: sco: fix UAF race in sco_conn_del

--- net/bluetooth/sco.c
+++ net/bluetooth/sco.c
@@ -189,22 +189,23 @@ static void sco_conn_del(struct hci_conn *hcon, int err)
 
 	BT_DBG("hcon %p conn %p, err %d", hcon, conn, err);
 
+	/* Ensure no more work items will run before freeing conn. */
+	cancel_delayed_work_sync(&conn->timeout_work);
+
 	/* Kill socket */
 	sco_conn_lock(conn);
 	sk = conn->sk;
+	if (sk)
+		sock_hold(sk);
 	sco_conn_unlock(conn);
 
 	if (sk) {
-		sock_hold(sk);
 		bh_lock_sock(sk);
 		sco_sock_clear_timer(sk);
 		sco_chan_del(sk, err);
 		bh_unlock_sock(sk);
 		sock_put(sk);
 	}
-
-	/* Ensure no more work items will run before freeing conn. */
-	cancel_delayed_work_sync(&conn->timeout_work);
 
 	hcon->sco_data = NULL;
 	kfree(conn);
